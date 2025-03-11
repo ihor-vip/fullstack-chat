9bcd04475441c433079fd1c6e56016572ee087af
Ihor Ploskodnyak <vip_anonymous@Ihors-Mac-mini.local>
2025-03-11 14:02:33 -0500

    socket implementation

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,11 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-
-const app = express();
-const PORT = process.env.PORT || 8000;
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
+
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,11 +20,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server Running on port ${PORT}`);
 });
